4b1fbe4f554a282c3a7dfadf989408dd6c275296
Laberon <laberon83@gmail.com>
2022-11-07 14:23:23 +0300

    Start

---
+++ src/main/java/Laberon/coursework/Courseworksecondyear/CourseworkSecondYearApplication.java
@@ -0,0 +1,13 @@
+package Laberon.coursework.Courseworksecondyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkSecondYearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkSecondYearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Laberon/coursework/Courseworksecondyear/CourseworkSecondYearApplicationTests.java
@@ -0,0 +1,13 @@
+package Laberon.coursework.Courseworksecondyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkSecondYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
